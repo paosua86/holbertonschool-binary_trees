368e1e2348dc5a211b420d173cb53520c10bb67d
paosua86 <paolasuarezb1986@gmail.com>
2022-09-09 15:54:14 -0500

    11 working

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,9 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through a binary
- * tree using in-order traversal
- * @tree: is a pointer to the root node of the tree to traverse
- * @func: is a pointer to a function to call for each node
- * Return: 1 if node is a leaf, otherwise 0
+ * binary_tree_size - function that measures the size of
+ * a binary tree
+ * @tree: is a pointer to the root node of the tree to measure the size
+ * Return: If tree is NULL, the function must return 0
  */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+	return (1 + left + right);
+}
