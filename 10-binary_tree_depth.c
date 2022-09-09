9df09d87a38207d66cbb01f7edd313fe24607944
paosua86 <paolasuarezb1986@gmail.com>
2022-09-09 15:15:12 -0500

    10 working

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,9 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through a binary
- * tree using in-order traversal
- * @tree: is a pointer to the root node of the tree to traverse
- * @func: is a pointer to a function to call for each node
+ * binary_tree_depth -  function that measures the depth of a node in a binary tree
+ * @tree: is a pointer to the node to measure the depth
  * Return: 1 if node is a leaf, otherwise 0
  */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (tree == NULL)
+		return (depth);
+
+	for (depth = 0; tree->parent != NULL; depth++)
+	{
+		tree = tree->parent;
+	}
+	return (depth);
+}
