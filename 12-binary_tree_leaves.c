174cd92043e207c7277820bdf12845e7233f82a1
root <root@LAPTOP-8V50EPRG.localdomain>
2022-09-12 11:48:11 -0500

    12 working

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,9 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through a binary
- * tree using in-order traversal
- * @tree: is a pointer to the root node of the tree to traverse
- * @func: is a pointer to a function to call for each node
- * Return: 1 if node is a leaf, otherwise 0
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the number of leaves
+ * Return: If tree is NULL, the function must return 
  */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (1);
+
+	else
+		return (2); 
+}
