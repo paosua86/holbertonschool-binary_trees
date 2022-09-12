5f6fd3e2bf05f383171165da6bbde63ed5871d18
root <root@LAPTOP-8V50EPRG.localdomain>
2022-09-12 12:04:11 -0500

    12 working

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,12 +8,16 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t left, right;
+	
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL || tree->right == NULL)
 		return (1);
 
-	else
-		return (2); 
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+	
+	return (left + right); 
 }
